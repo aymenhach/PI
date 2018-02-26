dcde5dbde775731f84ff86bca40765d56e106d37
aymenhach <33961213+aymenhach@users.noreply.github.com>
2018-02-26 18:40:03 +0100

    Add files via upload

--- WebPi/se_connecter.php
+++ WebPi/se_connecter.php
@@ -7,14 +7,14 @@ $pwd = $_POST['password'];
 $req = $db->query("SELECT * FROM user WHERE email like '%$email%'");
 $user=$req->fetch();
 if ($user['email']==NULL){
-     header('Location: http://localhost/webpi/login.html');
+     header('Location: login.html');
 }
 else if ($user['email']==$email && $pwd == $user['pwd']) {
      $_SESSION['loggeduser'] = $user['id'];
-     header('Location: http://localhost/webpi/accueil.php');
+     header('Location: accueil.php');
 }
 else {
-    	 header('Location: http://localhost/webpi/login.html');
+    	 header('Location: login.html');
 }
 
 ?>
